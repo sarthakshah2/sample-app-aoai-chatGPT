42996859059476388665a14ec579614bb00f7e25
NancyOsalaniya <69629001+NancyOsalaniya@users.noreply.github.com>
2024-02-27 17:16:05 +0530

    Add files via upload

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -79,7 +79,7 @@ const Chat = () => {
   const [clearingChat, setClearingChat] = useState<boolean>(false);
   const [hideErrorDialog, { toggle: toggleErrorDialog }] = useBoolean(true);
   const [errorMsg, setErrorMsg] = useState<ErrorMessage | null>();
-  let userName;
+  const [userName, setUserName] = useState("");
 
   const errorDialogContentProps = {
     type: DialogType.close,
@@ -134,16 +134,18 @@ const Chat = () => {
       return;
     }
     const userInfoList = await getUserInfo();
-    console.log("UserInfoList: " + userInfoList);
+    console.log("Chat UserInfoList: " + userInfoList);
 
     let nameObj = userInfoList[0].user_claims.find(
       (claim) => claim.typ === "name"
     );
-    console.log("nameObj: " + nameObj);
+    console.log("Chat nameObj: " + nameObj);
 
-    userName = nameObj ? nameObj.val : "Default Name";
+    setUserName(nameObj ? nameObj.val : "Default Name");
+    // userName = nameObj ? nameObj.val : "Default Name";
 
-    console.log("UserName: " + userName);
+    console.log("Chat UserName: " + userName);
+
     if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
       setShowAuthMessage(true);
     } else {
@@ -702,6 +704,8 @@ const Chat = () => {
         ChatHistoryLoadingState.Loading
     );
   };
+
+  console.log("Chat UserName: " + userName);
 
   return (
     <div className={styles.container} role="main">
