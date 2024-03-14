04e3edfc5b9c99b27b39795a9989b7cdc1aa520c
Parth0424 <109300028+Parth0424@users.noreply.github.com>
2024-03-14 15:24:17 +0530

    Add files via upload

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -72,7 +72,7 @@ const Layout = () => {
                 src={Contoso}
                 className={styles.headerIcon}
                 aria-hidden="true"
-                alt="eInfochips"
+                alt="Eragon"
               />
             </div>
 
@@ -80,10 +80,10 @@ const Layout = () => {
               <Link
                 to="/"
                 className={styles.headerTitleContainer}
-                aria-label="eInfochips Copilot - FAE's Personal AI Assistant"
+                aria-label="Eragon Copilot - FAE's Personal AI Assistant"
               >
                 <h1 className={styles.headerTitle}>
-                  eInfochips Copilot - FAE's Personal AI Assistant
+                  Eragon Copilot - FAE's Personal AI Assistant
                 </h1>
               </Link>
             </div>
